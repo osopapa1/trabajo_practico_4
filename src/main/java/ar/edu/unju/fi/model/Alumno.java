3280e7886d3ebaa5d57b583dda7681395055567b
chano <chano@LAPTOP-VDHN2S85>
2024-06-04 17:51:50 -0300

    Parte 01 avanzada

---
+++ src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -0,0 +1,91 @@
+package ar.edu.unju.fi.model;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String telefono;
+	private LocalDate fechaDeNac;
+	private String domicilio;
+	private String LU;
+	
+	public Alumno(String dni, String nombre, String apellido, String email, String telefono, LocalDate fechaDeNac,
+			String domicilio, String lU) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.fechaDeNac = fechaDeNac;
+		this.domicilio = domicilio;
+		LU = lU;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFechaDeNac() {
+		return fechaDeNac;
+	}
+
+	public void setFechaDeNac(LocalDate fechaDeNac) {
+		this.fechaDeNac = fechaDeNac;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public String getLU() {
+		return LU;
+	}
+
+	public void setLU(String lU) {
+		LU = lU;
+	}
+}
---
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -0,0 +1,82 @@
+package ar.edu.unju.fi.model;
+
+import ar.edu.unju.fi.constantes.Modalidad;
+
+public class Materia {
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private byte cantHoras;
+	private Modalidad modalidad;
+	private Docente docente;
+	private Carrera carrera;
+	
+	public Materia(String codigo, String nombre, String curso, byte cantHoras, Modalidad modalidad, Docente docente,
+			Carrera carrera) {
+		super();
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.curso = curso;
+		this.cantHoras = cantHoras;
+		this.modalidad = modalidad;
+		this.docente = docente;
+		this.carrera = carrera;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public byte getCantHoras() {
+		return cantHoras;
+	}
+
+	public void setCantHoras(byte cantHoras) {
+		this.cantHoras = cantHoras;
+	}
+
+	public Modalidad getModalidad() {
+		return modalidad;
+	}
+
+	public void setModalidad(Modalidad modalidad) {
+		this.modalidad = modalidad;
+	}
+
+	public Docente getDocente() {
+		return docente;
+	}
+
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+
+	public Carrera getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+	
+}
